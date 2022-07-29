847298b1635254147d8d4aad78aa59ecb3965b23
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2022-07-29 18:15:03 +0530

    reverse a stack

---
+++ Stacks/Reverse a Stack/ReverseAStack.h
@@ -0,0 +1,11 @@
+void reverseStack(stack<int> &input, stack<int> &extra)
+{
+
+    while (!input.empty())
+    {
+        int x = input.top();
+        input.pop();
+        extra.push(x);
+    }
+    input = extra;
+}
---
+++ Stacks/Reverse a Stack/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+#include "ReverseAStack.h"
+
+int main()
+{
+    stack<int> input, extra;
+    int size;
+    cin >> size;
+
+    for (int i = 0, val; i < size; i++)
+    {
+        cin >> val;
+        input.push(val);
+    }
+
+    reverseStack(input, extra);
+
+    while (!input.empty())
+    {
+        cout << input.top() << " ";
+        input.pop();
+    }
+}
