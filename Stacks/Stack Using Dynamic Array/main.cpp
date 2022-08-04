26a555c008e1ba3f0aa5fe21ddbee775a25a9c3f
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2022-08-04 14:49:34 +0530

    stack using dynamic array

---
+++ Stacks/Stack Using Dynamic Array/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "StackUsingDynamicArray.h"
+
+using namespace std;
+
+int main()
+{
+    StackUsingArray s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+
+    cout << s.top() << endl;
+
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+
+    cout << s.size() << endl;
+    cout << s.isEmpty() << endl;
+}
